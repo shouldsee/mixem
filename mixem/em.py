2bd9dd01cc1cc217259f7d754298217f57916d3f
shouldsee <shouldsee@github.com>
2019-01-18 14:25:04 +0000

    refactored callback and stopping criteria

--- mixem/em.py
+++ mixem/em.py
@@ -19,7 +19,15 @@ def logsumexp(X,axis=None,keepdims=1,log=1):
         S = S*np.exp(xmax)
     return S
 
-def em(data, distributions, initial_weights=None, max_iterations=100, tol=1e-15, tol_iters=10, progress_callback=mixem.simple_progress):
+def em(data, distributions, initial_weights=None, 
+       max_iterations=100, 
+       tol=1e-15, 
+       max_iters = None,
+       min_iters = 0,
+       tol_iters=10,
+       sample_weights= None, init_resp= None,
+       fix_weights = None,
+       progress_callback=mixem.simple_progress):
     """Fit a mixture of probability distributions using the Expectation-Maximization (EM) algorithm.
 
     :param data: The data to fit the distributions for. Can be an array-like or a :class:`numpy.ndarray`
@@ -45,7 +53,8 @@ def em(data, distributions, initial_weights=None, max_iterations=100, tol=1e-15,
 
     :rtype: tuple (weights, distributitons, log_likelihood)
     """
-
+    if max_iters is not None:
+        max_iterations = max_iters
     n_distr = len(distributions)
     n_data = data.shape[0]
 
@@ -53,49 +62,87 @@ def em(data, distributions, initial_weights=None, max_iterations=100, tol=1e-15,
         weight = np.array(initial_weights)
     else:
         weight = np.ones((n_distr,))
+    if sample_weights is not None:
+        sample_weights = sample_weights / sample_weights.mean()
 
     last_ll = np.zeros((tol_iters, ))
-    resp = np.empty((n_data, n_distr))
     log_density = np.empty((n_data, n_distr))
-
+    
+    def _m_step():
+        # M-step #######
+        for d in range(n_distr):
+#             if d in idx_active:
+            if idx_active[d]:
+                distributions[d].estimate_parameters(data, resp[:, d])    
+            
+    def weight_resp(resp):
+        if sample_weights is not None:
+            wresp = resp * sample_weights[:,None]
+            pass
+        else:
+            wresp = resp
+        return wresp
+            
     iteration = 0
+    idx_active = range(n_distr)
+    if init_resp is None:
+        resp = np.empty((n_data, n_distr))
+    else:
+        resp = init_resp
+        _m_step()
+    wresp  = weight_resp(resp)
+    
     while True:
+        
         # E-step #######
-
         # compute responsibilities
         for d in range(n_distr):
             log_density[:, d] = distributions[d].log_density(data)
 
         # normalize responsibilities of distributions so they sum up to one for example
-        logResp = np.log(weight[None,: ]) + log_density
-        logResp = logResp - logsumexp(logResp,axis=1)
-        resp = np.exp(logResp)
+        
+        log_proba = logProba = np.log(weight[None,: ]) + log_density
+        logResp = logProba - logsumexp(logProba,axis=1)
+        resp  = np.exp(logResp)
+        
+        sumWeight = resp.sum(axis=0)
+        idx_active = sumWeight != 0.
+        resp[:, ~ idx_active ] = 0.
+        wresp = weight_resp(resp)
+
+#         .nonzero()[0]
+#         assert (resp.sum(axis=0) != 0).all()
 #         resp = weight[np.newaxis, :] * np.exp(log_density)
 #         resp /= np.sum(resp, axis=1)[:,  np.newaxis]
 
-        log_likelihood = np.sum( resp * log_density)
+        log_likelihood = np.sum(( wresp[:,idx_active] * log_density[:,idx_active]))
 
-        # M-step #######
-        for d in range(n_distr):
-            distributions[d].estimate_parameters(data, resp[:, d])
-
-        weight = np.mean(resp, axis=0)
+        _m_step()
+        
+        if not fix_weights:
+            weight = np.mean(wresp, axis=0)
+#             weight = weight * 0. + 1.
 
         if progress_callback:
-            progress_callback(iteration, weight, distributions, log_likelihood)
-
+            res = progress_callback(iteration, weight, distributions, log_likelihood, log_proba)
+            if res is not None:
+                iteration = res[0]
+                
         # Convergence check #######
         if np.isnan(log_likelihood):
-            last_ll[0] = log_likelihood
-            break
+            iteration = -1
 
-        if iteration >= tol_iters and (last_ll[-1] - log_likelihood) / last_ll[-1] <= tol:
-            last_ll[0] = log_likelihood
-            break
+        if iteration >= max(min_iters,tol_iters) \
+        and (last_ll[-1] - log_likelihood) / last_ll[-1] <= tol:
+            iteration = -1
 
         if iteration >= max_iterations:
+            iteration = -1
+            
+        if iteration < 0:
             last_ll[0] = log_likelihood
-            break
+            res = progress_callback(iteration, weight, distributions, log_likelihood, log_proba)
+            break 
 
         # store value of current iteration in last_ll[0]
         # and shift older values to the right
--- mixem/progress.py
+++ mixem/progress.py
@@ -1,11 +1,21 @@
 # coding=utf-8
 
 
-def simple_progress(iteration, weights, distributions, log_likelihood):
+def simple_progress(iteration, weights, distributions, log_likelihood, log_proba):
     """A simple default progress callback to use with mixem.em"""
 
     print("iteration {iteration:4d} (log-likelihood={log_likelihood:.5e}): p(x|Φ) = {formatted_distributions}".format(
         iteration=iteration,
         log_likelihood=log_likelihood,
         formatted_distributions=" + ".join("{w:.3g}*{d}".format(w=w, d=d) for w, d in zip(weights, distributions))
+    ))
+    
+def very_simple_progress(iteration, weights, distributions, log_likelihood, log_proba):
+    """A simple default progress callback to use with mixem.em"""
+
+    print("iteration {iteration:4d} (log-likelihood={log_likelihood:.5e}): p(x|Φ) = {formatted_distributions}".format(
+        iteration=iteration,
+        log_likelihood=log_likelihood,
+        formatted_distributions=" + ".join("{w:.3g}*{d}".format(w=w, d=d) for w, d in zip(weights, distributions))[:10] 
+        
     ))
